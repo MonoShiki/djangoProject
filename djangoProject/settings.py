9c9697fc15dd9dd80ac6df1c6e2c56de3e6838e6
MonoShiki <91622816+MonoShiki@users.noreply.github.com>
2024-11-25 16:51:19 +0300

    Update settings.py

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['djangoproject-gvn3.onrender.com', 'localhost', '127.0.0.1']
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
